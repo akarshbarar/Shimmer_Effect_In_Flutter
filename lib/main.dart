4fefe0c4294fcf5d712f51917619b00f11faf368
Akarsh Barar <barar.akarsh8@gmail.com>
2019-10-30 13:25:45 +0530

    Shimmer Effect in Fluuter

--- lib/main.dart
+++ lib/main.dart
@@ -25,23 +25,21 @@ class _IndexState extends State<Index> {
         title: Text("Shimmer"),
       ),
       body: Center(
-        child: SizedBox(
-          width: 200.0,
-          height: 100.0,
-          child: Shimmer.fromColors(
-            baseColor: Colors.black,
-            highlightColor: Colors.grey,
-            child: Text(
-              'Shimmer',
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                fontSize: 40.0,
-                fontWeight:
-                FontWeight.bold,
-              ),
+        child: ListView.builder(
+                itemCount: 10,
+                itemBuilder: (context,i){
+                  return Shimmer.fromColors(
+                      child:  ListTile(
+                        title: Text("Hii",style: TextStyle(fontSize: 25,fontWeight: FontWeight.bold),),
+                        subtitle: Text(i.toString()),
+                      ),
+                      baseColor:Colors.black,
+                      highlightColor: Colors.grey
+                  );
+                }
             ),
-          ),
-        ),
+
+
       ),
     );
   }
